8f02825584504166068c47e2619348ec4c181d85
chinmay0722 <93027770+chinmay0722@users.noreply.github.com>
2024-04-12 16:34:15 +0530

    stream2.py

--- stream2.py
+++ stream2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.model_selection import train_test_split
 import joblib
 from acb import ccsc,aprc,aprdrgc
